8ebb1d0aae4810324ffeaa29b6a1c922b4379eda
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-15 14:04:51 -0700

    Added access token to /fbdisconnect. Removed del statements from /fbdisconnect, now done in /disconnect.

--- project.py
+++ project.py
@@ -243,14 +243,10 @@ def fbconnect():
 @app.route('/fbdisconnect')
 def fbdisconnect():
     facebook_id = login_session['facebook_id']
-    url = 'https://graph.facebook.com/%s/permissions' % facebook_id
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
-    del login_session['username']
-    del login_session['email']
-    del login_session['picture']
-    del login_session['user_id']
-    del login_session['facebook_id']
     return "you have been logged out"
 
 @app.route('/disconnect')
