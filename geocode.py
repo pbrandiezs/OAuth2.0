4640bd10505f4d2117c51fb2b5cb7e0bf5d6b059
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-19 16:50:10 -0700

    feat: Added geocode.py
    Added geocode.py as an example of a python API call to googlemaps api.

---
+++ geocode.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import httplib2
+import json
+
+def getGeocodeLocation(inputString):
+    google_api_key = "AIzaSyACNqkxg-l2HSJAQWIYDY4Xv_MfxCJYv5s"
+    locationString = inputString.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
+    h = httplib2.Http()
+    response, content = h.request(url, 'GET')
+    result = json.loads(content)
+    print "response header: %s \n \n" % response
+    return result
+
+print(getGeocodeLocation("Miami, Florida"))
