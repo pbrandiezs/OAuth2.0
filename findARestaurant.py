513fc7cf560948f0ffa19f452165b37bb5a249ae
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-21 08:14:10 -0700

    fix: Added back in additional restaurants to check

--- findARestaurant.py
+++ findARestaurant.py
@@ -111,12 +111,12 @@ def findARestaurant(mealType,location):
         return restaurantInfo
 
 if __name__ == '__main__':
-	# findARestaurant("Pizza", "Tokyo, Japan")
-	# findARestaurant("Tacos", "Jakarta, Indonesia")
-	# findARestaurant("Tapas", "Maputo, Mozambique")
-	# findARestaurant("Falafel", "Cairo, Egypt")
-	# findARestaurant("Spaghetti", "New Delhi, India")
-	# findARestaurant("Cappuccino", "Geneva, Switzerland")
+	findARestaurant("Pizza", "Tokyo, Japan")
+	findARestaurant("Tacos", "Jakarta, Indonesia")
+	findARestaurant("Tapas", "Maputo, Mozambique")
+	findARestaurant("Falafel", "Cairo, Egypt")
+	findARestaurant("Spaghetti", "New Delhi, India")
+	findARestaurant("Cappuccino", "Geneva, Switzerland")
 	findARestaurant("Sushi", "Los Angeles, California")
-	# findARestaurant("Steak", "La Paz, Bolivia")
+	findARestaurant("Steak", "La Paz, Bolivia")
 	findARestaurant("Gyros", "Sydney, Australia")
